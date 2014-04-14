f3402959fd701cfa0f809f83b12c715b8b226960
Rob Reynolds <ferventcoder@gmail.com>
2014-04-14 17:46:10 -0500

    (maint) capitalize chocolatey name

--- src/chocolatey/infrastructure.app/ApplicationParameters.cs
+++ src/chocolatey/infrastructure.app/ApplicationParameters.cs
@@ -5,7 +5,7 @@ namespace chocolatey.infrastructure.app
 
     public static class ApplicationParameters
     {
-        public static string Name = "chocolatey";
+        public static string Name = "Chocolatey";
         public static readonly string LoggingFile = @"chocolatey.log";
         public static readonly string Log4NetConfigurationAssembly = @"chocolatey";
         public static readonly string Log4NetConfigurationResource = @"chocolatey.infrastructure.logging.log4net.config.xml";
