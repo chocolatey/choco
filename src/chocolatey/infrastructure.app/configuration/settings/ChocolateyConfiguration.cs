3b61e71633623dfb0a7ea8ebe497c2de0ed029b2
Rob Reynolds <ferventcoder@gmail.com>
2014-04-15 15:49:37 -0500

    (config) add custom configuration for chocolatey

---
+++ src/chocolatey/infrastructure.app/configuration/settings/ChocolateyConfiguration.cs
@@ -0,0 +1,39 @@
+﻿namespace chocolatey.infrastructure.app.configuration.settings
+{
+    using System.Configuration;
+
+    public sealed class ChocolateyConfiguration : ConfigurationSection
+    {
+        static readonly ChocolateyConfiguration _settings =
+           ConfigurationManager.GetSection("chocolatey") as ChocolateyConfiguration;
+
+        public static ChocolateyConfiguration settings
+        {
+            get { return _settings; }
+        }
+
+        [ConfigurationProperty("sources", IsRequired = true, IsDefaultCollection = true)]
+        public SourcesConfigurationCollection sources
+        {
+            get { return (SourcesConfigurationCollection)this["sources"]; }
+        }
+
+        [ConfigurationProperty("useNuGetForSources", IsRequired = false, DefaultValue = false)]
+        public bool useNuGetForSources
+        {
+            get { return (bool)this["useNuGetForSources"]; }
+        }  
+        
+        [ConfigurationProperty("checksumFiles", IsRequired = false, DefaultValue = true)]
+        public bool checksumFiles
+        {
+            get { return (bool)this["checksumFiles"]; }
+        }
+        
+        [ConfigurationProperty("virusCheck", IsRequired = false, DefaultValue = false)]
+        public bool virusCheck
+        {
+            get { return (bool)this["virusCheck"]; }
+        }
+    }
+}
---
+++ src/chocolatey/infrastructure.app/configuration/settings/SourcesConfigurationCollection.cs
@@ -0,0 +1,22 @@
+﻿namespace chocolatey.infrastructure.app.configuration.settings
+{
+    using System.Configuration;
+
+    public sealed class SourcesConfigurationCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new SourceConfigurationElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return element;
+        }
+
+        public SourceConfigurationElement Item(int index)
+        {
+            return (SourceConfigurationElement)(base.BaseGet(index));
+        }
+    }
+}
