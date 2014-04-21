1947fdf70c2f4e5083c6e4b721400e31618bfc34
Rob Reynolds <ferventcoder@gmail.com>
2014-04-21 15:07:39 -0500

    (event) more to event messages
    Not a fan of the way this is setup yet.

--- src/chocolatey/infrastructure.app/messages/PreRunEvent.cs
+++ src/chocolatey/infrastructure.app/messages/PreRunEvent.cs
@@ -3,8 +3,27 @@
     using events;
     using infrastructure.commands;
 
-    public class PreRunEvent<Command> : IEvent where Command: ICommand
+    public class PreRunEvent<TCommand> : IEvent where TCommand : ICommand
     {
-         
+        public TCommand Command { get; private set; }
+        public object[] State { get; private set; }
+
+        public PreRunEvent(TCommand command, object[] state)
+        {
+            Command = command;
+            State = state;
+        }
+    }
+
+    public class PostRunEvent<TCommand> : IEvent where TCommand : ICommand
+    {
+        public TCommand Command { get; private set; }
+        public object[] State { get; private set; }
+
+        public PostRunEvent(TCommand command, object[] state)
+        {
+            Command = command;
+            State = state;
+        }
     }
 }
