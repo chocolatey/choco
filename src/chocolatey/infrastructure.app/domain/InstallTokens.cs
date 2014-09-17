2ad6e7a2a9d5539ff2274fedb924d11480df8651
Rob Reynolds <ferventcoder@gmail.com>
2014-09-17 13:51:57 -0500

    adding InstallTokens b/c magic strings...

---
+++ src/chocolatey/infrastructure.app/domain/InstallTokens.cs
@@ -0,0 +1,11 @@
+﻿namespace chocolatey.infrastructure.app.domain
+{
+    public static class InstallTokens
+    {
+        public const string INSTALLER_LOCATION = "{{INSTALLER_LOCATION}}";
+        public const string CUSTOM_INSTALL_LOCATION = "{{CUSTOM_INSTALL_LOCATION}}";
+        public const string PACKAGE_LOCATION = "{{PACKAGE_LOCATION}}";
+        public const string LANGUAGE = "{{LANGUAGE}}";
+        public const string UNINSTALLER_LOCATION = "{{UNINSTALLER_LOCATION}}";
+    }
+}
