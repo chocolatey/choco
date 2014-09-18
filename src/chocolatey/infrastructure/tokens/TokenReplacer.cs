5103e0289aa252f5cda8938b582a2386f392ae42
Rob Reynolds <ferventcoder@gmail.com>
2014-09-18 17:22:21 -0500

    (infra) tokenreplacer can replace against any type
    This makes token replacer super smart, you can pass it any type with properties
    and it will do token replacement based on the name of those properties and
    their values.

--- src/chocolatey/infrastructure/tokens/TokenReplacer.cs
+++ src/chocolatey/infrastructure/tokens/TokenReplacer.cs
@@ -3,16 +3,15 @@ namespace chocolatey.infrastructure.tokens
     using System.Collections.Generic;
     using System.Reflection;
     using System.Text.RegularExpressions;
-    using app.configuration;
 
     public sealed class TokenReplacer
     {
-        public static string replace_tokens(ChocolateyConfiguration configuration, string textToReplace)
+        public static string replace_tokens<TConfig>(TConfig configuration, string textToReplace, string tokenPrefix = "\\[\\[", string tokenSuffix = "\\]\\]")
         {
             if (string.IsNullOrEmpty(textToReplace)) return string.Empty;
 
             IDictionary<string, string> dictionary = create_dictionary_from_configuration(configuration);
-            var regex = new Regex("{{(?<key>\\w+)}}");
+            var regex = new Regex("{0}(?<key>\\w+){1}".format_with(tokenPrefix,tokenSuffix));
 
             string output = regex.Replace(textToReplace, m =>
                 {
@@ -21,7 +20,7 @@ namespace chocolatey.infrastructure.tokens
                     key = m.Groups["key"].Value.to_lower();
                     if (!dictionary.ContainsKey(key))
                     {
-                        return "{{" + key + "}}";
+                        return tokenPrefix + key + tokenSuffix;
                     }
 
                     string value = dictionary[key];
@@ -31,7 +30,7 @@ namespace chocolatey.infrastructure.tokens
             return output;
         }
 
-        private static IDictionary<string, string> create_dictionary_from_configuration(ChocolateyConfiguration configuration)
+        private static IDictionary<string, string> create_dictionary_from_configuration<TConfig>(TConfig configuration)
         {
             var propertyDictionary = new Dictionary<string, string>();
             foreach (PropertyInfo property in configuration.GetType().GetProperties())
