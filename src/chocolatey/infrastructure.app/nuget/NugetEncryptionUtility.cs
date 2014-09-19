172c6e8dfcbaedc582bf159f9377cdea9f328a61
Rob Reynolds <ferventcoder@gmail.com>
2014-09-19 08:28:47 -0500

    (nuget) encryption utility

---
+++ src/chocolatey/infrastructure.app/nuget/NugetEncryptionUtility.cs
@@ -0,0 +1,40 @@
+﻿namespace chocolatey.infrastructure.app.nuget
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+    using NuGet;
+
+    // ReSharper disable InconsistentNaming
+
+    public static class NugetEncryptionUtility
+    {
+        private static readonly byte[] _entropyBytes = Encoding.UTF8.GetBytes("Chocolatey");
+
+        internal static string EncryptString(string value)
+        {
+            var decryptedByteArray = Encoding.UTF8.GetBytes(value);
+            var encryptedByteArray = ProtectedData.Protect(decryptedByteArray, _entropyBytes, DataProtectionScope.LocalMachine);
+            var encryptedString = Convert.ToBase64String(encryptedByteArray);
+            return encryptedString;
+        }
+
+        internal static string DecryptString(string encryptedString)
+        {
+            var encryptedByteArray = Convert.FromBase64String(encryptedString);
+            var decryptedByteArray = ProtectedData.Unprotect(encryptedByteArray, _entropyBytes, DataProtectionScope.LocalMachine);
+            return Encoding.UTF8.GetString(decryptedByteArray);
+        }
+
+        public static string GenerateUniqueToken(string caseInsensitiveKey)
+        {
+            // SHA256 is case sensitive; given that our key is case insensitive, we upper case it
+            var pathBytes = Encoding.UTF8.GetBytes(caseInsensitiveKey.ToUpperInvariant());
+            var hashProvider = new CryptoHashProvider("SHA256");
+
+            return Convert.ToBase64String(hashProvider.CalculateHash(pathBytes)).ToUpperInvariant();
+        }
+    }
+
+    // ReSharper restore InconsistentNaming
+}
