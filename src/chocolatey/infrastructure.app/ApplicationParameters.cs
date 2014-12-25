4ef529b2f20bc35a8de7bfb39839f628a2e5308e
Rob Reynolds <ferventcoder@gmail.com>
2014-12-25 17:29:43 -0600

    (maint) update install location for all paths

--- src/chocolatey/infrastructure.app/ApplicationParameters.cs
+++ src/chocolatey/infrastructure.app/ApplicationParameters.cs
@@ -12,15 +12,16 @@ namespace chocolatey.infrastructure.app
     {
         private static readonly IFileSystem _fileSystem = new DotNetFileSystem();
         public static readonly string Name = "Chocolatey";
-        public static readonly string InstallLocation = _fileSystem.get_directory_name(Assembly.GetExecutingAssembly().Location);
+        //public static readonly string InstallLocation = Environment.GetEnvironmentVariable("ChocolateyInstall") ??  _fileSystem.get_directory_name(Assembly.GetExecutingAssembly().Location);
+        public static readonly string InstallLocation =  _fileSystem.get_directory_name(Assembly.GetExecutingAssembly().Location);
         public static readonly string CommonAppDataChocolatey = _fileSystem.combine_paths(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), Name);
-        public static readonly string LoggingLocation = _fileSystem.combine_paths(CommonAppDataChocolatey, "logs");
+        public static readonly string LoggingLocation = _fileSystem.combine_paths(InstallLocation, "logs");
         public static readonly string LoggingFile = @"chocolatey.log";
         public static readonly string Log4NetConfigurationAssembly = @"chocolatey";
         public static readonly string Log4NetConfigurationResource = @"chocolatey.infrastructure.logging.log4net.config.xml";
         public static readonly string ChocolateyConfigFileResource = @"chocolatey.infrastructure.app.configuration.chocolatey.config";
-        public static readonly string GlobalConfigFileLocation = _fileSystem.combine_paths(CommonAppDataChocolatey, "config", "chocolatey.config");
-        public static readonly string LicenseFileLocation = _fileSystem.combine_paths(CommonAppDataChocolatey, "license", "chocolatey.license.xml");
+        public static readonly string GlobalConfigFileLocation = _fileSystem.combine_paths(InstallLocation, "config", "chocolatey.config");
+        public static readonly string LicenseFileLocation = _fileSystem.combine_paths(InstallLocation, "license", "chocolatey.license.xml");
         public static readonly string PackageNamesSeparator = ";";
 
         public static string PackagesLocation = _fileSystem.combine_paths(InstallLocation, "lib");
