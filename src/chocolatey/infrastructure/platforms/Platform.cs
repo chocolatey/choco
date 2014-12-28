fd94dfe980fa80b058aa5a6b3372781382ed61b4
Rob Reynolds <ferventcoder@gmail.com>
2014-12-27 18:34:07 -0600

    (refactor) Platform
    - Using IFileSystem instead of straight directory calls
    - specs to prove out behavior

--- src/chocolatey/infrastructure/platforms/Platform.cs
+++ src/chocolatey/infrastructure/platforms/Platform.cs
@@ -2,18 +2,23 @@ namespace chocolatey.infrastructure.platforms
 {
     using System;
     using System.ComponentModel;
-    using System.IO;
     using adapters;
     using filesystem;
     using Environment = adapters.Environment;
 
+    /// <summary>
+    ///   OS Platform detection
+    /// </summary>
+    /// <remarks>
+    ///   Based on http://stackoverflow.com/questions/10138040/how-to-detect-properly-windows-linux-mac-operating-systems
+    /// </remarks>
     public static class Platform
     {
         private static Lazy<IEnvironment> environment_initializer = new Lazy<IEnvironment>(() => new Environment());
         private static Lazy<IFileSystem> file_system_initializer = new Lazy<IFileSystem>(() => new DotNetFileSystem());
 
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public static void initialize_with(Lazy<IEnvironment> environment,Lazy<IFileSystem> file_system)
+        public static void initialize_with(Lazy<IEnvironment> environment, Lazy<IFileSystem> file_system)
         {
             environment_initializer = environment;
             file_system_initializer = file_system;
@@ -24,7 +29,8 @@ namespace chocolatey.infrastructure.platforms
             get { return file_system_initializer.Value; }
         }
 
-        private static IEnvironment Environment {
+        private static IEnvironment Environment
+        {
             get { return environment_initializer.Value; }
         }
 
@@ -32,20 +38,19 @@ namespace chocolatey.infrastructure.platforms
         {
             switch (Environment.OSVersion.Platform)
             {
-                case PlatformID.Unix:
-                    // Well, there are chances MacOSX is reported as Unix instead of MacOSX.
-                    // Instead of platform check, we'll do a feature checks (Mac specific root folders)
-                    if (Directory.Exists("/Applications")
-                        & Directory.Exists("/System")
-                        & Directory.Exists("/Users")
-                        & Directory.Exists("/Volumes"))
-                        return PlatformType.Mac;
-                    else
-                        return PlatformType.Linux;
-
                 case PlatformID.MacOSX:
                     return PlatformType.Mac;
 
+                case PlatformID.Unix:
+                    // Well, there are chances MacOSX is reported as Unix instead of MacOSX.
+                    // Instead of platform check, we'll do a feature checks (Mac specific root folders)
+                    if (file_system.directory_exists("/Applications")
+                        & file_system.directory_exists("/System")
+                        & file_system.directory_exists("/Users")
+                        & file_system.directory_exists("/Volumes"))
+                        return PlatformType.Mac;
+                    else
+                        return PlatformType.Linux;
                 default:
                     return PlatformType.Windows;
             }
