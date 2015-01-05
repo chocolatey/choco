81b9ec56806750db6605d82626007220deeee4b6
Rob Reynolds <ferventcoder@gmail.com>
2015-01-05 11:29:31 -0600

    (maint) todo integration tests

---
+++ src/chocolatey.tests.integration/TODO.cs
@@ -0,0 +1,30 @@
+﻿namespace chocolatey.tests.integration
+{
+    public class TODO
+    {
+        /*
+         * Configuration related:
+         *  - value set automatically
+         *  - value overridden
+         *  
+         * Install/Upgrade/Uninstall scenarios:
+         *  - happy path
+         *  - install, then upgrade happy path
+         *  - sxs to normal
+         *  - normal to sxs
+         *  - installed, force install
+         *  - install noop
+         *  - upgrade noop
+         *  - uninstall noop
+         *  - uninstall happy path
+         *  
+         * List scenarios:
+         *  - list local
+         *  - list lp
+         *  
+         * New scenarios:
+         *  - run happy path
+         * 
+         */ 
+    }
+}
