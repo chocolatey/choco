f12fd9850112e6f623c121bc2fea54c6bd98854f
Rob Reynolds <ferventcoder@gmail.com>
2015-01-11 17:04:07 -0600

    (bug) fix bad assembly.cs

--- src/chocolatey.tests/Properties/AssemblyInfo.cs
+++ src/chocolatey.tests/Properties/AssemblyInfo.cs
@@ -13,15 +13,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-n this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
-    [assembly: ComVisible(false)]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("chocolatey.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("chocolatey.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2012 - Present")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-    [assembly: Guid("0f612899-20f0-45e0-890a-068aa22339d7")]
+[assembly: Guid("0f612899-20f0-45e0-890a-068aa22339d7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +47,5 @@ n this assembly not visible
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-    [assembly: AssemblyVersion("1.0.0.0")]
-    [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
