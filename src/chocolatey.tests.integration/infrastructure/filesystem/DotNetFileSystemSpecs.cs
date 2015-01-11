9e56c0680e1f8b545c3f6c3ef71352bdc0c02f63
Rob Reynolds <ferventcoder@gmail.com>
2015-01-11 17:10:14 -0600

    (maint) formatted chocolatey.tests.integration

--- src/chocolatey.tests.integration/infrastructure/filesystem/DotNetFileSystemSpecs.cs
+++ src/chocolatey.tests.integration/infrastructure/filesystem/DotNetFileSystemSpecs.cs
@@ -1,4 +1,19 @@
-﻿namespace chocolatey.tests.integration.infrastructure.filesystem
+﻿// Copyright © 2011 - Present RealDimensions Software, LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// You may obtain a copy of the License at
+// 
+// 	http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.tests.integration.infrastructure.filesystem
 {
     using System;
     using System.IO;
@@ -64,23 +79,23 @@
             {
                 string filePath = FileSystem.combine_paths(ContextPath, "chocolateyInstall.ps1");
 
-                FileSystem.write_file(filePath,"yo");
+                FileSystem.write_file(filePath, "yo");
                 var actual = FileSystem.get_files(ContextPath, "chocolateyInstall.ps1", SearchOption.AllDirectories);
                 FileSystem.delete_file(filePath);
-                
+
                 actual.ShouldNotBeEmpty();
                 actual.Count.ShouldEqual(1);
-            }   
-            
+            }
+
             [Fact]
             public void GetFiles_should_return_files_that_meet_the_pattern_regardless_of_case()
             {
                 string filePath = FileSystem.combine_paths(ContextPath, "chocolateyInstall.ps1");
 
-                FileSystem.write_file(filePath,"yo");
+                FileSystem.write_file(filePath, "yo");
                 var actual = FileSystem.get_files(ContextPath, "chocolateyinstall.ps1", SearchOption.AllDirectories);
                 FileSystem.delete_file(filePath);
-                
+
                 actual.ShouldNotBeEmpty();
                 actual.Count.ShouldEqual(1);
             }
