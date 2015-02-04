3ce6e2ba7ece5ef039a9035ca6abc5fffb4a348b
Rob Reynolds <ferventcoder@gmail.com>
2015-02-04 01:07:32 -0600

    (GH-7) Install .NET Fx Full
    Instead of the client profile as folks have reported issues with the
    client profile being installed (for other things). Better to take the
    additional few megabyte hit of the full framework install to alleviate
    any issues.

--- nuget/chocolatey/tools/chocolateysetup.psm1
+++ nuget/chocolatey/tools/chocolateysetup.psm1
@@ -325,7 +325,7 @@ function Install-DotNet4IfMissing {
   if (!(test-path "$env:windir\Microsoft.Net\$fx\v4.0.30319")) {
     $NetFx4ClientUrl = 'http://download.microsoft.com/download/5/6/2/562A10F9-C9F4-4313-A044-9C94E0A8FAC8/dotNetFx40_Client_x86_x64.exe'
     $NetFx4FullUrl = 'http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe'
-    Install-ChocolateyPackage "NetFx4.0" 'exe' -silentArgs "/q /norestart /repair /log `'$tempDir\NetFx4Install.log`'" -url "$NetFx4ClientUrl" -url64bit "$NetFx4ClientUrl" -validExitCodes @(0, 3010)
+    Install-ChocolateyPackage "NetFx4.0" 'exe' -silentArgs "/q /norestart /repair /log `'$tempDir\NetFx4Install.log`'" -url "$NetFx4FullUrl" -url64bit "$NetFx4FullUrl" -validExitCodes @(0, 3010)
   }
 }
 
