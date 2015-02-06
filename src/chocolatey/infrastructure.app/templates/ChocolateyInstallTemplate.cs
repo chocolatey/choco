c2d3feac5b81823c97760c97be0c76b4a74eee46
Rob Reynolds <ferventcoder@gmail.com>
2015-02-05 21:41:27 -0600

    (GH-70) choco new generates uninstall template
    Generate the chocolateyuninstall.ps1 file as well when generating a
    package. This gives an example that someone can use to fill in the
    blanks.

--- src/chocolatey/infrastructure.app/templates/ChocolateyInstallTemplate.cs
+++ src/chocolatey/infrastructure.app/templates/ChocolateyInstallTemplate.cs
@@ -17,11 +17,12 @@ namespace chocolatey.infrastructure.app.templates
 {
     public class ChocolateyInstallTemplate
     {
-        public static string InstallerTemplate =
+        public static string Template =
             @"#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
 [[AutomaticPackageNotesInstaller]]
 $packageName = '[[PackageName]]' # arbitrary name for the package, used in messages
+$registryUninstallerKeyName = '[[PackageName]]' #ensure this is the value in the registry
 $installerType = '[[InstallerType]]' #only one of these: exe, msi, msu
 $url = '[[Url]]' # download url
 $url64 = '[[Url64]]' # 64bit URL here or remove - if installer decides, then use $url
@@ -29,8 +30,8 @@ $silentArgs = '[[SilentArgs]]' # ""/s /S /q /Q /quiet /silent /SILENT /VERYSILEN
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $toolsDir = ""$(Split-Path -parent $MyInvocation.MyCommand.Definition)""
 
-# main helpers - these have error handling tucked into them already
-# see https://github.com/chocolatey/chocolatey/wiki/HelpersReference
+# main helper functions - these have error handling tucked into them already
+# see https://github.com/chocolatey/choco/wiki/HelpersReference
 # Install an application, will assert administrative rights
 # add additional optional arguments as necessary
 #Install-ChocolateyPackage ""$packageName"" ""$installerType"" ""$silentArgs"" ""$url"" [""$url64""  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
@@ -42,42 +43,35 @@ Install-ChocolateyZipPackage ""$packageName"" ""$url"" ""$toolsDir""
 #Install-ChocolateyVsixPackage ""$packageName"" ""$url"" [$vsVersion] [-checksum $checksum -checksumType $checksumType]
 Install-ChocolateyVsixPackage ""$packageName"" ""$url""
 
-#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # see the full list at https://github.com/chocolatey/chocolatey/wiki/HelpersReference
-  # downloader that the main helpers use to download items
-  # if removing $url64, please remove from here
-  #Get-ChocolateyWebFile ""$packageName"" 'DOWNLOAD_TO_FILE_FULL_PATH' ""$url"" ""$url64""
-  # installer, will assert administrative rights - used by Install-ChocolateyPackage
-  #Install-ChocolateyInstallPackage ""$packageName"" ""$installerType"" ""$silentArgs"" '_FULLFILEPATH_' -validExitCodes $validExitCodes
-  # unzips a file to the specified location - auto overwrites existing content
-  #Get-ChocolateyUnzip ""FULL_LOCATION_TO_ZIP.zip"" ""$toolsDir""
-  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
-  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-  #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
-  # add specific files as shortcuts to the desktop
-  #$target = Join-Path $toolsDir ""$($packageName).exe""
-  #Install-ChocolateyDesktopLink $target
-  # outputs the bitness of the OS (either ""32"" or ""64"")
-  #$osBitness = Get-ProcessorBits
+# see the full list at https://github.com/chocolatey/choco/wiki/HelpersReference
+# downloader that the main helpers use to download items
+# if removing $url64, please remove from here
+#Get-ChocolateyWebFile ""$packageName"" 'DOWNLOAD_TO_FILE_FULL_PATH' ""$url"" ""$url64""
+# installer, will assert administrative rights - used by Install-ChocolateyPackage
+#Install-ChocolateyInstallPackage ""$packageName"" ""$installerType"" ""$silentArgs"" '_FULLFILEPATH_' -validExitCodes $validExitCodes
+# unzips a file to the specified location - auto overwrites existing content
+#Get-ChocolateyUnzip ""FULL_LOCATION_TO_ZIP.zip"" ""$toolsDir""
+# Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
+#Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
+# add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
+#Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
+# add specific files as shortcuts to the desktop
+#$target = Join-Path $toolsDir ""$($packageName).exe""
+#Install-ChocolateyDesktopLink $target
+# outputs the bitness of the OS (either ""32"" or ""64"")
+#$osBitness = Get-ProcessorBits
 
-  # PORTABLE EXAMPLE
-  #$binRoot = Get-BinRoot
-  #$installDir = Join-Path $binRoot ""$packageName""
-  #Write-Host ""Adding `'$installDir`' to the path and the current shell path""
-  #Install-ChocolateyPath ""$installDir""
-  #$env:Path = ""$($env:Path);$installDir""
+# PORTABLE EXAMPLE
+#$binRoot = Get-BinRoot
+#$installDir = Join-Path $binRoot ""$packageName""
+#Write-Host ""Adding `'$installDir`' to the path and the current shell path""
+#Install-ChocolateyPath ""$installDir""
+#$env:Path = ""$($env:Path);$installDir""
 
-  # if removing $url64, please remove from here
-  # despite the name ""Install-ChocolateyZipPackage"" this also works with 7z archives
-  Install-ChocolateyZipPackage ""$packageName"" ""$url"" ""$installDir"" ""$url64""
-  # END PORTABLE EXAMPLE
-
-  # everything below is all part of error handling
-#} catch {
-  #throw $_
-#}
+# if removing $url64, please remove from here
+# despite the name ""Install-ChocolateyZipPackage"" this also works with 7z archives
+Install-ChocolateyZipPackage ""$packageName"" ""$url"" ""$installDir"" ""$url64""
+# END PORTABLE EXAMPLE
 ";
 
         public static string AutomaticPackageNotes =
---
+++ src/chocolatey/infrastructure.app/templates/ChocolateyUninstallTemplate.cs
@@ -0,0 +1,42 @@
+﻿// Copyright © 2011 - Present RealDimensions Software, LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// You may obtain a copy of the License at
+// 
+// 	http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.infrastructure.app.templates
+{
+    public class ChocolateyUninstallTemplate
+    {
+        public static string Template =
+            @"#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+
+$packageName = '[[PackageName]]'
+$registryUninstallerKeyName = '[[PackageName]]' #ensure this is the value in the registry
+$installerType = 'EXE'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+#$osBitness = Get-ProcessorBits
+ 
+#if ($osBitness -eq 64) {
+#  $file = (Get-ItemProperty -Path ""hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"").UninstallString
+#} else {
+#  $file = (Get-ItemProperty -Path ""hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"").UninstallString
+#} 
+#	
+#Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+";
+    }
+}
