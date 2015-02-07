b475f97a801a29c4d1a447dd47317d16ca032107
Rob Reynolds <ferventcoder@gmail.com>
2015-02-06 18:28:19 -0600

    (build) fix customize to work properly

--- .build/customize.bat
+++ .build/customize.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 ::Project UppercuT - http://uppercut.googlecode.com
 ::No edits to this file are required - http://uppercut.pbwiki.com
@@ -15,11 +15,12 @@ SET step.name=%1
 SET step.type=%2
 
 
-SET DIR=%~d0%~p0%
-SET NANT="%DIR%..\lib\Nant\nant.exe"
-SET build.config.settings="%DIR%..\.uppercut"
+SET DIR=%cd%
+SET BUILD_DIR=%~d0%~p0%
+SET NANT="%DIR%\lib\Nant\nant.exe"
+SET build.config.settings="%DIR%\.uppercut"
 
-%NANT% /f:.\customize.build -D:build.config.settings=%build.config.settings% -D:customize.step.name=%step.name% -D:customize.step.type=%step.type%
+%NANT% /logger:"NAnt.Core.DefaultLogger" /quiet /nologo /f:"%BUILD_DIR%customize.build" -D:build.config.settings=%build.config.settings% -D:customize.step.name=%step.name% -D:customize.step.type=%step.type%
 
 if %ERRORLEVEL% NEQ 0 goto errors
 
@@ -27,7 +28,7 @@ goto finish
 
 :usage
 echo.
-echo Usage: customize.bat stepName customizeType
+echo Usage: .build\customize.bat stepName customizeType
 echo stepName is the name of the item
 echo customizeType is "pre" "post" or "replace"
 echo .
