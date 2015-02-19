28a63abf4b6a76a2bf382c31c05b4f7ca49aeb4f
Rob Reynolds <ferventcoder@gmail.com>
2015-02-19 13:14:48 -0600

    (GH-106) XmlService - Fault tolerance

--- src/chocolatey/infrastructure/services/XmlService.cs
+++ src/chocolatey/infrastructure/services/XmlService.cs
@@ -15,13 +15,12 @@
 
 namespace chocolatey.infrastructure.services
 {
-    using System;
     using System.IO;
     using System.Text;
     using System.Xml;
     using System.Xml.Serialization;
-    using configuration;
     using filesystem;
+    using tolerance;
 
     /// <summary>
     ///   XML interaction
@@ -37,47 +36,44 @@ namespace chocolatey.infrastructure.services
 
         public XmlType deserialize<XmlType>(string xmlFilePath)
         {
-            try
-            {
-                var xmlSerializer = new XmlSerializer(typeof (XmlType));
-                var xmlReader = XmlReader.Create(new StringReader(_fileSystem.read_file(xmlFilePath)));
-                if (!xmlSerializer.CanDeserialize(xmlReader))
-                {
-                    this.Log().Warn("Cannot deserialize response of type {0}", typeof (XmlType));
-                    return default(XmlType);
-                }
+            return FaultTolerance.try_catch_with_logging_exception(
+                () =>
+                    {
+                        var xmlSerializer = new XmlSerializer(typeof (XmlType));
+                        var xmlReader = XmlReader.Create(new StringReader(_fileSystem.read_file(xmlFilePath)));
+                        if (!xmlSerializer.CanDeserialize(xmlReader))
+                        {
+                            this.Log().Warn("Cannot deserialize response of type {0}", typeof (XmlType));
+                            return default(XmlType);
+                        }
 
-                return (XmlType) xmlSerializer.Deserialize(xmlReader);
-            }
-            catch (Exception ex)
-            {
-                this.Log().Error("Error deserializing response of type {0}:{1}{2}", typeof (XmlType), Environment.NewLine, ex.ToString());
-                throw;
-            }
+                        return (XmlType) xmlSerializer.Deserialize(xmlReader);
+                    },
+                "Error deserializing response of type {0}".format_with(typeof (XmlType)),
+                throwError: true);
         }
 
         public void serialize<XmlType>(XmlType xmlType, string xmlFilePath)
         {
             _fileSystem.create_directory_if_not_exists(_fileSystem.get_directory_name(xmlFilePath));
-            try
-            {
-                var xmlSerializer = new XmlSerializer(typeof (XmlType));
-                var textWriter = new StreamWriter(xmlFilePath, append: false, encoding: Encoding.UTF8)
+
+            FaultTolerance.try_catch_with_logging_exception(
+                () =>
                     {
-                        AutoFlush = true
-                    };
+                        var xmlSerializer = new XmlSerializer(typeof (XmlType));
+                        var textWriter = new StreamWriter(xmlFilePath, append: false, encoding: Encoding.UTF8)
+                            {
+                                AutoFlush = true
+                            };
 
-                xmlSerializer.Serialize(textWriter, xmlType);
-                textWriter.Flush();
+                        xmlSerializer.Serialize(textWriter, xmlType);
+                        textWriter.Flush();
 
-                textWriter.Close();
-                textWriter.Dispose();
-            }
-            catch (Exception ex)
-            {
-                this.Log().Error("Error serializing type {0}:{1}{2}", typeof (XmlType), Environment.NewLine, Config.get_configuration_settings().Debug ? ex.ToString() : ex.Message);
-                throw;
-            }
+                        textWriter.Close();
+                        textWriter.Dispose();
+                    },
+                "Error serializing type {0}".format_with(typeof (XmlType)),
+                throwError: true);
         }
     }
 }
