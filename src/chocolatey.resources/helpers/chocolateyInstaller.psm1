chocolatey/choco

commit abe92f3f86b0fd0ce5cd2ace7a87b89edde0a5df

(GH-207) Some pkgs incorrectly use arguments
* Set $installArguments to the environment variable
* Set $forcex86 to the environment variable
* Set $packageParameters to the environment variable
* Set $overrideArgs to the environment variable

src/chocolatey.resources/helpers/chocolateyInstaller.psm1
@@ -19,6 +19,16 @@ if ($env:ChocolateyEnvironmentDebug -eq 'true') { $DebugPreference = "Continue";
 $VerbosePreference = "SilentlyContinue"
 if ($env:ChocolateyEnvironmentVerbose -eq 'true') { $VerbosePreference = "Continue"; $verbosity = $true }
 
+$installArguments = $env:chocolateyInstallArguments
+
+$overrideArgs = false
+if ($env:chocolateyInstallOverride -eq 'true') { $overrideArgs = true }
+
+$forceX86 = false
+if ($env:chocolateyForceX86 -eq 'true') { $forceX86 = true }
+
+$packageParameters = $env:chocolateyPackageParameters
+
 # ensure module loading preference is on
 $PSModuleAutoLoadingPreference = "All";
 

src/chocolatey/infrastructure.app/services/PowershellService.cs
@@ -160,8 +160,10 @@ namespace chocolatey.infrastructure.app.services
                 Environment.SetEnvironmentVariable("packageVersion", package.Version.to_string());
                 Environment.SetEnvironmentVariable("chocolateyPackageFolder", packageDirectory);
                 Environment.SetEnvironmentVariable("packageFolder", packageDirectory);
+                Environment.SetEnvironmentVariable("installArguments", configuration.InstallArguments);
                 Environment.SetEnvironmentVariable("installerArguments", configuration.InstallArguments);
                 Environment.SetEnvironmentVariable("chocolateyInstallArguments", configuration.InstallArguments);
+                Environment.SetEnvironmentVariable("packageParameters", configuration.PackageParameters);
                 Environment.SetEnvironmentVariable("chocolateyPackageParameters", configuration.PackageParameters);
                 if (configuration.ForceX86)
                 {

commit 45ca6272f0314997c555836af7635068dc41119c

(GH-215) The term 'false' is not recognized
A regression introduced in
https://github.com/chocolatey/choco/commit/abe92f3f86b0fd0ce5cd2ace7a87b89edde0a5df
which is just in 0.9.9.3 that was attempting to solve other issues
caused choco to stop working completely in some versions of PowerShell,
which have stricter requirements for setting booleans.

Without this fix, chocolatey will continue to be unusable

src/chocolatey.resources/helpers/chocolateyInstaller.psm1
@@ -21,11 +21,11 @@ if ($env:ChocolateyEnvironmentVerbose -eq 'true') { $VerbosePreference = "Contin
 
 $installArguments = $env:chocolateyInstallArguments
 
-$overrideArgs = false
-if ($env:chocolateyInstallOverride -eq 'true') { $overrideArgs = true }
+$overrideArgs = $false
+if ($env:chocolateyInstallOverride -eq 'true') { $overrideArgs = $true }
 
-$forceX86 = false
-if ($env:chocolateyForceX86 -eq 'true') { $forceX86 = true }
+$forceX86 = $false
+if ($env:chocolateyForceX86 -eq 'true') { $forceX86 = $true }
 
 $packageParameters = $env:chocolateyPackageParameters
 