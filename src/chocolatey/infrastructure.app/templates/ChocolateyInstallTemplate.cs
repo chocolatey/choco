050ce144c39b83ef2feb855ff80637e53f84a6df
Rob Reynolds <ferventcoder@gmail.com>
2015-07-25 09:13:33 -0500

    (GH-354) Fix template MSI Install Options
    The path to the log file must be double quoted and exist. Using
    apostrophes causes the install to fail.

--- src/chocolatey/infrastructure.app/templates/ChocolateyInstallTemplate.cs
+++ src/chocolatey/infrastructure.app/templates/ChocolateyInstallTemplate.cs
@@ -37,7 +37,7 @@ $packageArgs = @{
   url64bit      = $url64
 
   #MSI
-  silentArgs    = ""/qn /norestart /l*v '$env:TEMP\chocolatey\$packageName\install.log'"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = ""/qn /norestart /l*v `""$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"""" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
   #OTHERS
   #silentArgs    ='[[SilentArgs]]' # ""/s /S /q /Q /quiet /silent /SILENT /VERYSILENT -s"" # try any of these to get the silent installer
