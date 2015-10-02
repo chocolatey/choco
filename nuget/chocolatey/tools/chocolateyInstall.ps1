62dab723d776d0dac827a6db1da6aab971bf5139
Rich Siegel <rsiegel@ise.com>
2015-10-02 11:31:16 -0400

    (GH-442) add conditional to prevent removal of unloaded modules
    this is part of a cleaning up content that might be contained
    within stderr.

--- nuget/chocolatey/tools/chocolateyInstall.ps1
+++ nuget/chocolatey/tools/chocolateyInstall.ps1
@@ -7,7 +7,9 @@ $modules = Get-ChildItem $toolsPath -Filter *.psm1
 $modules | ForEach-Object {
 														$psm1File = $_.FullName;
 														$moduleName = $([System.IO.Path]::GetFileNameWithoutExtension($psm1File))
-														remove-module $moduleName -ErrorAction SilentlyContinue;
+														if (Get-Module $moduleName) {
+                              remove-module $moduleName -ErrorAction SilentlyContinue;
+                            }
 														import-module -name  $psm1File;
 													}
 
