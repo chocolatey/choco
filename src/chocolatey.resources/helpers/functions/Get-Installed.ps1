2f4f5ba880cea551ace87f2891bee3bc77890883
den_po <jdp@bk.ru>
2015-10-18 23:30:37 +0300

    (GH-396) Get-Installed helper
    Direct replacement for "Get-WmiObject -Class Win32_Product".
    Win32_Product does lots of unnecessary work and may be VERY slow. Some
    of packages use it to check if a package is already installed.
    
    This change adds Get-Installed helper and a hook that processes
    Win32_Product class requests.

---
+++ src/chocolatey.resources/helpers/functions/Get-Installed.ps1
@@ -0,0 +1,48 @@
+﻿## Get-Installed
+##############################################################################################################
+## Returns a list of installed applications.
+## Registry based replacement for "Get-WmiObject -Class Win32_Product".
+##############################################################################################################
+
+function Get-Installed {
+param(
+  [switch] $GWMICompatible = $false # You may use '-GWMICompatible' switch
+)
+
+  $items = @()
+
+  if(Test-Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall") {
+    $items += Get-ChildItem "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+  }
+  if(Test-Path "HKLM:SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall") {
+    $items += Get-ChildItem "HKLM:SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+  }
+
+  $productList = @()
+
+  foreach ($app in $items)
+  {
+    $propitems = $app | Foreach-Object {Get-ItemProperty $_.PsPath}
+    $w32item = New-Object System.Object
+    $w32item | Add-Member -type NoteProperty -Name "IdentifyingNumber" -Value $app.PsChildname
+    $w32item | Add-Member -type NoteProperty -Name "Name"    -Value $propitems.DisplayName
+    $w32item | Add-Member -type NoteProperty -Name "Vendor"  -Value $propitems.Publisher
+    $w32item | Add-Member -type NoteProperty -Name "Version" -Value $propitems.DisplayVersion
+    $w32item | Add-Member -type NoteProperty -Name "Caption" -Value $propitems.DisplayName
+
+    if($w32item.Name) {
+
+      if(-not $GWMICompatible) {
+        $w32item | Add-Member -type NoteProperty -Name "WindowsInstaller" -Value $propitems.WindowsInstaller
+        $w32item | Add-Member -type NoteProperty -Name "UninstallString" -Value $propitems.UninstallString
+      }
+
+      if((-not $GWMICompatible) -or ($propitems.WindowsInstaller -eq "1")) {
+        $productList += $w32item
+      }
+
+    }
+  }
+
+  return $productList
+}
