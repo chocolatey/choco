ab82a7877718548bbe6af297083e1226ab2743f3
Rob Reynolds <ferventcoder@gmail.com>
2016-01-02 12:07:24 -0600

    (GH-525) Improve Write-Progress
    As a continuation of GH-8, download progress should be written inline,
    but needs some improvements:
    - Write an empty line when it has completed so that the next log item
       doesn't write to the same line.
    - Improve the content of the progress message with both percentage and
      the status message.

--- src/chocolatey/infrastructure/powershell/PoshHostUserInterface.cs
+++ src/chocolatey/infrastructure/powershell/PoshHostUserInterface.cs
@@ -118,14 +118,27 @@ namespace chocolatey.infrastructure.powershell
             this.Log().Debug(message);
         }
 
+        private bool hasLoggedStartProgress = false;
+        private bool hasLoggedFinalProgress = false;
         public override void WriteProgress(long sourceId, ProgressRecord record)
-        {
+        {  
             if (record.PercentComplete == -1) return;
-
-            if (record.PercentComplete == 100) this.Log().Debug(() => "Progress: 100%{0}".format_with(" ".PadRight(20)));
+            if (hasLoggedFinalProgress) return;
+            if (!hasLoggedStartProgress)
+            {
+                hasLoggedStartProgress = true;
+                this.Log().Debug(record.Activity);
+            }
 
             // http://stackoverflow.com/a/888569/18475
-            Console.Write("\rProgress: {0}%{1}".format_with(record.PercentComplete, " ".PadRight(20)));
+            Console.Write("\rProgress: {0}% - {1}".format_with(record.PercentComplete.to_string(), record.StatusDescription.PadRight(50, ' ')));
+
+            if (record.PercentComplete == 100 && !hasLoggedFinalProgress)
+            {
+                hasLoggedFinalProgress = true;
+                this.Log().Info("");
+                this.Log().Info(record.StatusDescription.Replace("Saving","Finished downloading. Saved"));
+            }
         }
 
         public override void WriteVerboseLine(string message)
