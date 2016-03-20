b480d6b0d5700ff0478a4eb24b6d88dc0eedf3d7
Rob Reynolds <ferventcoder@gmail.com>
2016-03-20 10:11:37 -0500

    (GH-412)(GH-664) PoSh tab completion / refreshenv
    Provide PowerShell tab completion for Chocolatey commands including
    
    - searching for installed packages for upgrade/uninstall
    - searching for packages to install  - only return the top 5 results
       from a search and only search by id
    
    Also set refreshenv as part of the profile additions.

---
+++ src/chocolatey.resources/helpers/chocolateyProfile.psm1
@@ -0,0 +1,9 @@
+﻿# ensure module loading preference is on
+$PSModuleAutoLoadingPreference = "All";
+
+$thisDirectory = (Split-Path -parent $MyInvocation.MyCommand.Definition);
+. $thisDirectory\functions\Get-EnvironmentVariable.ps1
+. $thisDirectory\functions\Get-EnvironmentVariableNames.ps1
+. $thisDirectory\functions\Update-SessionEnvironment.ps1
+. $thisDirectory\ChocolateyTabExpansion.ps1
+Export-ModuleMember -Alias refreshenv -Function 'Update-SessionEnvironment', 'TabExpansion'
