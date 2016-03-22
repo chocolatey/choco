fc2f0942df7ebb69f6eede62eba006aaaeca3b52
Rob Reynolds <ferventcoder@gmail.com>
2016-03-22 14:54:32 -0500

    (GH-412) Add licensing to new Posh files

--- src/chocolatey.resources/helpers/ChocolateyTabExpansion.ps1
+++ src/chocolatey.resources/helpers/ChocolateyTabExpansion.ps1
@@ -1,3 +1,18 @@
+# Copyright © 2011 - Present RealDimensions Software, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Ideas from the Awesome Posh-Git - https://github.com/dahlbyk/posh-git
 # Posh-Git License - https://github.com/dahlbyk/posh-git/blob/1941da2472eb668cde2d6a5fc921d5043a024386/LICENSE.txt
 # http://www.jeremyskinner.co.uk/2010/03/07/using-git-with-windows-powershell/
