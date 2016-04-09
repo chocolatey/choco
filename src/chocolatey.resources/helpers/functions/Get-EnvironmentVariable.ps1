9bbfb4e882111bffb5f910b267f0e09ad10b95af
Fernando Tirolo Gomes <tirolo@gmail.com>
2016-04-09 12:42:08 -0500

    (GH-303) Get-EnvironmentVariable Changes
    - Declare argument as `switch` instead of `bool`.
    - Renamed some variables and added comments.
    - Updated references to Get-EnvironmentVariable.
    - Cleaned up formatting.
    - Allowed getting process scoped environment variables again.

--- src/chocolatey.resources/helpers/functions/Get-EnvironmentVariable.ps1
+++ src/chocolatey.resources/helpers/functions/Get-EnvironmentVariable.ps1
@@ -12,25 +12,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function Get-EnvironmentVariable([string] $Name, [System.EnvironmentVariableTarget] $Scope, [bool] $PreserveVariables = $False) {
-    if ($pathType -eq [System.EnvironmentVariableTarget]::Machine) {
-        $reg = [Microsoft.Win32.Registry]::Machine.OpenSubKey("Environment", $true)
-    } else {
-        $reg = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey("Environment", $true)
-    }
+function Get-EnvironmentVariable {
+<#
+.SYNOPSIS
+Gets an Environment Variable.
 
-    if ($PreserveVariables -eq $True) {
-        $option = [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames
-    } else {
-        $option = [Microsoft.Win32.RegistryValueOptions]::None
-    }
+.DESCRIPTION
+This will will get an environment variable based on the variable name and scope while accounting whether to expand the variable or not (e.g.: %TEMP% -> C:\User\Username\AppData\Local\Temp).
 
-    $value = $reg.GetValue('Path', $null, $option)
+.PARAMETER Name
+The environemnt variable you want to get the value from.
 
-    $reg.Close()
+.PARAMETER Scope
+The environemnt variable target scope.
 
-    return $value
+.PARAMETER PreserveVariables
+A switch parameter stating whether you want to expand the variables or not. Defaults to false.
+
+.EXAMPLE
+Get-EnvironmentVariable 'TEMP' User -PreserveVariables
+
+.NOTES
+This helper reduces the number of lines one would have to write to get environment variables, mainly when not expanding the variables is a must.
+#>
+[CmdletBinding()]
+[OutputType([string])]
+param(
+  [Parameter(Mandatory=$true)]
+  [string] $Name, 
+  [Parameter(Mandatory=$true)]
+  [System.EnvironmentVariableTarget] $Scope, 
+  [Parameter(Mandatory=$false)]
+  [switch] $PreserveVariables = $false
+)
+  [string] $MACHINE_ENVIRONMENT_REGISTRY_KEY_NAME = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment\";
+  [Microsoft.Win32.RegistryKey] $win32RegistryKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey($MACHINE_ENVIRONMENT_REGISTRY_KEY_NAME)
+  if ($Scope -eq [System.EnvironmentVariableTarget]::User) {
+    [string] $USER_ENVIRONMENT_REGISTRY_KEY_NAME = "Environment";
+    [Microsoft.Win32.RegistryKey] $win32RegistryKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey($USER_ENVIRONMENT_REGISTRY_KEY_NAME)
+  } elseif ($Scope -eq [System.EnvironmentVariableTarget]::Process) {
+    [Environment]::GetEnvironmentVariable($Name, $Scope)
+  }
+
+  [Microsoft.Win32.RegistryValueOptions] $registryValueOptions = [Microsoft.Win32.RegistryValueOptions]::None
+
+  if ($PreserveVariables) {
+    Write-Verbose "Choosing not to expand environment names"
+    $registryValueOptions = [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames
+  }
+
+  [string] $environmentVariableValue = [string]::Empty
+
+  try {
+    Write-Verbose "Getting environment variable $Name"
+    $environmentVariableValue = $win32RegistryKey.GetValue($Name, [string]::Empty, $registryValueOptions)
+  } catch {
+    Write-Debug "Unable to retrieve the $Name environment variable. Details: $_"
+  } finally {
+    $win32RegistryKey.Close()
+  }
+
+  return $environmentVariableValue
 }
-
-# Some enhancements to think about here.
-# $machinePath = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Control\Session Manager\Environment\").GetValue("PATH", "", [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames).ToString();
