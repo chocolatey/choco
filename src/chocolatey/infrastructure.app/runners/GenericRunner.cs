ff97904b5c398349b738e16e643fc996c33c73f2
Rob Reynolds <ferventcoder@gmail.com>
2016-09-05 16:12:11 -0500

    (maint) formatting

--- src/chocolatey/infrastructure.app/runners/GenericRunner.cs
+++ src/chocolatey/infrastructure.app/runners/GenericRunner.cs
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 namespace chocolatey.infrastructure.app.runners
 {
     using System;
