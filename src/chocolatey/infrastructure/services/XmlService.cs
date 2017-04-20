1414aafb74a5a033e5c7611d8ebd5410f05038ff
Manfred Wallner <schusterfredl@mwallner.net>
2017-04-20 09:47:41 +0200

    (GH-1258) Global mutex for config
    Previously, multiple Chocolatey processes running could affect or
    corrupt the chocolatey.config file depending on how they accessed the
    file. Implement a single global mutex (process-wide) on serialization and
    deserialization of the xml configuration file to ensure the file stays
    consistent.

--- src/chocolatey/infrastructure/services/XmlService.cs
+++ src/chocolatey/infrastructure/services/XmlService.cs
@@ -24,6 +24,7 @@ namespace chocolatey.infrastructure.services
     using cryptography;
     using filesystem;
     using tolerance;
+    using chocolatey.infrastructure.synchronization;
 
     /// <summary>
     ///   XML interaction
@@ -32,6 +33,7 @@ namespace chocolatey.infrastructure.services
     {
         private readonly IFileSystem _fileSystem;
         private readonly IHashProvider _hashProvider;
+        private const int MUTEX_TIMEOUT = 1000;
 
         public XmlService(IFileSystem fileSystem, IHashProvider hashProvider)
         {
@@ -41,110 +43,120 @@ namespace chocolatey.infrastructure.services
 
         public XmlType deserialize<XmlType>(string xmlFilePath)
         {
-            return FaultTolerance.try_catch_with_logging_exception(
+            return FaultTolerance.retry(3, () => GlobalMutex.enter(
                 () =>
                 {
-                    var xmlSerializer = new XmlSerializer(typeof(XmlType));
-                    using (var fileStream = _fileSystem.open_file_readonly(xmlFilePath))
-                    using (var fileReader = new StreamReader(fileStream))
-                    using (var xmlReader = XmlReader.Create(fileReader))
+                    return FaultTolerance.try_catch_with_logging_exception(
+                    () =>
                     {
-                        if (!xmlSerializer.CanDeserialize(xmlReader))
+                        var xmlSerializer = new XmlSerializer(typeof(XmlType));
+                        using (var fileStream = _fileSystem.open_file_readonly(xmlFilePath))
+                        using (var fileReader = new StreamReader(fileStream))
+                        using (var xmlReader = XmlReader.Create(fileReader))
                         {
-                            this.Log().Warn("Cannot deserialize response of type {0}", typeof(XmlType));
-                            return default(XmlType);
-                        }
-
-                        try 
-                        {
-                            return (XmlType)xmlSerializer.Deserialize(xmlReader);
-                        } 
-                        catch(InvalidOperationException ex)
-                        {
-                            // Check if its just a malformed document.
-                            if (ex.Message.Contains("There is an error in XML document"))
+                            if (!xmlSerializer.CanDeserialize(xmlReader))
                             {
-                                // If so, check for a backup file and try an parse that.
-                                if (_fileSystem.file_exists(xmlFilePath + ".backup"))
+                                this.Log().Warn("Cannot deserialize response of type {0}", typeof(XmlType));
+                                return default(XmlType);
+                            }
+
+                            try
+                            {
+                                return (XmlType)xmlSerializer.Deserialize(xmlReader);
+                            }
+                            catch (InvalidOperationException ex)
+                            {
+                                // Check if its just a malformed document.
+                                if (ex.Message.Contains("There is an error in XML document"))
                                 {
-                                    using (var backupStream = _fileSystem.open_file_readonly(xmlFilePath + ".backup"))
-                                    using (var backupReader = new StreamReader(backupStream))
-                                    using (var backupXmlReader = XmlReader.Create(backupReader))
+                                    // If so, check for a backup file and try an parse that.
+                                    if (_fileSystem.file_exists(xmlFilePath + ".backup"))
                                     {
-                                        var validConfig = (XmlType)xmlSerializer.Deserialize(backupXmlReader);
-
-                                        // If there's no errors and it's valid, go ahead and replace the bad file with the backup.
-                                        if(validConfig != null)
+                                        using (var backupStream = _fileSystem.open_file_readonly(xmlFilePath + ".backup"))
+                                        using (var backupReader = new StreamReader(backupStream))
+                                        using (var backupXmlReader = XmlReader.Create(backupReader))
                                         {
-                                            _fileSystem.copy_file(xmlFilePath + ".backup", xmlFilePath, overwriteExisting: true);
-                                        }
+                                            var validConfig = (XmlType)xmlSerializer.Deserialize(backupXmlReader);
 
-                                        return validConfig;
+                                            // If there's no errors and it's valid, go ahead and replace the bad file with the backup.
+                                            if (validConfig != null)
+                                            {
+                                                _fileSystem.copy_file(xmlFilePath + ".backup", xmlFilePath, overwriteExisting: true);
+                                            }
+
+                                            return validConfig;
+                                        }
                                     }
                                 }
+
+                                throw;
                             }
- 
-                            throw;
                         }
-                    }
-                },
-                "Error deserializing response of type {0}".format_with(typeof(XmlType)),
-                throwError: true);
+                    },
+                    "Error deserializing response of type {0}".format_with(typeof(XmlType)),
+                    throwError: true);
+
+                }, MUTEX_TIMEOUT));
         }
 
         public void serialize<XmlType>(XmlType xmlType, string xmlFilePath)
         {
-            serialize(xmlType,xmlFilePath, isSilent: false);
+            serialize(xmlType, xmlFilePath, isSilent: false);
         }
 
         public void serialize<XmlType>(XmlType xmlType, string xmlFilePath, bool isSilent)
         {
             _fileSystem.create_directory_if_not_exists(_fileSystem.get_directory_name(xmlFilePath));
 
-            FaultTolerance.try_catch_with_logging_exception(
+            FaultTolerance.retry(3, () => GlobalMutex.enter(
                 () =>
                 {
-                    var xmlSerializer = new XmlSerializer(typeof(XmlType));
-
-                    // Write the updated file to memory
-                    using(var memoryStream = new MemoryStream())                        
-                    using(var streamWriter = new StreamWriter(memoryStream, encoding: new UTF8Encoding(encoderShouldEmitUTF8Identifier: true)))
+                    FaultTolerance.try_catch_with_logging_exception(
+                    () =>
                     {
-                        xmlSerializer.Serialize(streamWriter, xmlType);
-                        streamWriter.Flush();
+                        var xmlSerializer = new XmlSerializer(typeof(XmlType));
 
-                        memoryStream.Position = 0;
-                        
-                        // Grab the hash of both files and compare them.
-                        var originalFileHash = _hashProvider.hash_file(xmlFilePath);
-                        if (!originalFileHash.is_equal_to(_hashProvider.hash_stream(memoryStream)))
+                        // Write the updated file to memory
+                        using (var memoryStream = new MemoryStream())
+                        using (var streamWriter = new StreamWriter(memoryStream, encoding: new UTF8Encoding(encoderShouldEmitUTF8Identifier: true)))
                         {
-                            // If there wasn't a file there in the first place, just write the new one out directly.
-                            if(string.IsNullOrEmpty(originalFileHash))
+                            xmlSerializer.Serialize(streamWriter, xmlType);
+                            streamWriter.Flush();
+
+                            memoryStream.Position = 0;
+
+                            // Grab the hash of both files and compare them.
+                            var originalFileHash = _hashProvider.hash_file(xmlFilePath);
+                            if (!originalFileHash.is_equal_to(_hashProvider.hash_stream(memoryStream)))
                             {
-                                using(var updateFileStream = _fileSystem.create_file(xmlFilePath))
+                                // If there wasn't a file there in the first place, just write the new one out directly.
+                                if (string.IsNullOrEmpty(originalFileHash))
+                                {
+                                    using (var updateFileStream = _fileSystem.create_file(xmlFilePath))
+                                    {
+                                        memoryStream.Position = 0;
+                                        memoryStream.CopyTo(updateFileStream);
+                                        _fileSystem.write_file(xmlFilePath, () => memoryStream);
+
+                                        return;
+                                    }
+                                }
+
+                                // Otherwise, create an update file, and resiliently move it into place.
+                                var tempUpdateFile = xmlFilePath + ".update";
+                                using (var updateFileStream = _fileSystem.create_file(tempUpdateFile))
                                 {
                                     memoryStream.Position = 0;
                                     memoryStream.CopyTo(updateFileStream);
-
-                                    return;
                                 }
+                                _fileSystem.replace_file(tempUpdateFile, xmlFilePath, xmlFilePath + ".backup");
                             }
-
-                            // Otherwise, create an update file, and resiliently move it into place.
-                            var tempUpdateFile = xmlFilePath + ".update";
-                            using(var updateFileStream = _fileSystem.create_file(tempUpdateFile))
-                            {
-                                memoryStream.Position = 0;
-                                memoryStream.CopyTo(updateFileStream);
-                            }
-                            _fileSystem.replace_file(tempUpdateFile, xmlFilePath, xmlFilePath + ".backup");
                         }
-                    }
-                },
-                "Error serializing type {0}".format_with(typeof(XmlType)),
-                throwError: true, 
-                isSilent: isSilent);
+                    },
+                    "Error serializing type {0}".format_with(typeof(XmlType)),
+                    throwError: true,
+                    isSilent: isSilent);
+                }, MUTEX_TIMEOUT));
         }
     }
 }
