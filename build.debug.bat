2738a8c709c2bfeab7c4ecdc2744fe5eed468255
Rob Reynolds <ferventcoder@gmail.com>
2017-05-15 13:40:33 -0500

    (build) option to build debug version
    Provide an option to build debugging versions of Chocolatey. This
    should not be used in production scenarios as Chocolatey uses a different
    path versus ChocolateyInstall environment variable to determine where it
    is and could lead to very unexpected and hard to find bugs.

---
+++ build.debug.bat
@@ -0,0 +1,35 @@
+@echo off
+
+::Project UppercuT - http://uppercut.googlecode.com
+::No edits to this file are required - http://uppercut.pbwiki.com
+
+if '%1' == '/?' goto usage
+if '%1' == '-?' goto usage
+if '%1' == '?' goto usage
+if '%1' == '/help' goto usage
+
+SET DIR=%cd%
+SET BUILD_DIR=%~d0%~p0%
+SET NANT="%BUILD_DIR%lib\Nant\nant.exe"
+SET build.config.settings="%DIR%\.uppercut"
+
+%NANT% /logger:"NAnt.Core.DefaultLogger" /quiet /nologo /f:"%BUILD_DIR%.build\default.build" -D:build.config.settings=%build.config.settings% -D:msbuild.configuration="Debug" -D:run.nuget="false" -D:test.framework="None" -D:cover.framework="None" -D:run.codesign="false" %*
+
+::#-D:run.ilmerge="false"
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
+call %DIR%\documentscenarios.bat
+
+goto finish
+
+:usage
+echo.
+echo Usage: build.bat
+echo.
+goto finish
+
+:errors
+EXIT /B %ERRORLEVEL%
+
+:finish
