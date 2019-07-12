560ecf17b5595999f2d2ce0db2ab8948ca1268dc
Matthew D. Pagel <MPagel@users.noreply.github.com>
2019-07-12 13:17:45 -0700

    changed URL reference
    made a few whitespace changes.

--- src/chocolatey.resources/redirects/RefreshEnv.cmd
+++ src/chocolatey.resources/redirects/RefreshEnv.cmd
@@ -15,14 +15,14 @@ REM It also sets "convenience" PATH_HKCU and PATH_HKLM environment variables.
 echo Refreshing environment variables from registry to active cmd.exe session.
 echo ** WARNING **
 echo If using PowerShell, do not use this script. Use refreshenv from Chocolatey (see
-echo chocolatey.org/docs/troubleshooting#i-cant-get-the-powershell-tab-completion-working)
+echo https://chocolatey.org/docs/troubleshooting#refreshenv-has-no-effect)
 
 GOTO main
 
 REM CALLd procedures.
 REM Set one environment variable from registry key
 :SetFromReg
-    "%WinDir%\System32\Reg" QUERY "%~1" /v "%~2" > "%TEMP%\_envset.tmp" 2>NUL
+    "%WinDir%\System32\Reg" QUERY "%~1" /v "%~2" >"%TEMP%\_envset.tmp" 2>NUL
     for /f "usebackq skip=2 tokens=2,*" %%A IN ("%TEMP%\_envset.tmp") do (
         REM If blank, don't erase the variable. Maybe force-set to space? Alt-255?
         IF /I NOT "%%~B"=="" (
@@ -38,7 +38,7 @@ REM Set one environment variable from registry key
 
 REM Get a list of environment variables from registry
 :GetRegEnv
-    "%WinDir%\System32\Reg" QUERY "%~1" > "%TEMP%\_envget.tmp"
+    "%WinDir%\System32\Reg" QUERY "%~1" >"%TEMP%\_envget.tmp"
     for /f "usebackq skip=2" %%A IN ("%TEMP%\_envget.tmp") do (
         if /I not "%%~A"=="Path" (
             call :SetFromReg "%~1" "%%~A" "%%~A"
@@ -51,14 +51,14 @@ REM Processing begin
     echo/@echo off >"%TEMP%\_env.cmd"
 
     REM Generate batch file, one line at a time first from System then User
-    call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" >> "%TEMP%\_env.cmd"
-    call :GetRegEnv "HKCU\Environment" >> "%TEMP%\_env.cmd"
+    call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" >>"%TEMP%\_env.cmd"
+    call :GetRegEnv "HKCU\Environment" >>"%TEMP%\_env.cmd"
 
     REM Special handling for PATH - mix both User and System
-    call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path_HKLM >> "%TEMP%\_env.cmd"
-    call :SetFromReg "HKCU\Environment" Path Path_HKCU >> "%TEMP%\_env.cmd"
+    call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path_HKLM >>"%TEMP%\_env.cmd"
+    call :SetFromReg "HKCU\Environment" Path Path_HKCU >>"%TEMP%\_env.cmd"
     call set "EXPATH=%%%%Path_HKLM%%%%;%%%%Path_HKCU%%%%"
-    echo/call set "Path=!EXPATH!">> "%TEMP%\_env.cmd"
+    echo/call set "Path=!EXPATH!" >>"%TEMP%\_env.cmd"
 
     REM Remove EXPATH, TEMPVAR, etc
     endlocal
