e3950d0a5125f2bdd140f145cfbf0d416f20c557
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-10-28 21:43:09 -0500

    (#62) Add unit tests for DefaultPushSource
    Adds tests for DefaultPushSource to check it works correctly.
    
    If DefaultPushSource is set, it should set the source assuming that
    a source is not passed in explicitly with --source. If a source is
    passed in explicitly, then DefaultPushSource should not override it.
    
    If DefaultPushSource is set to "disabled", it should throw an error if
    there is not an explicit source passed in. But if an explicit source is
    passed in, it should not error.
    
    If the DefaultPushSource is set to the name/alias of a machine source,
    it should resolve to that machine source.

--- src/chocolatey.tests/infrastructure.app/commands/ChocolateyPushCommandSpecs.cs
+++ src/chocolatey.tests/infrastructure.app/commands/ChocolateyPushCommandSpecs.cs
@@ -147,6 +147,7 @@ namespace chocolatey.tests.infrastructure.app.commands
             {
                 reset();
                 configuration.Sources = "";
+                configuration.PushCommand.DefaultSource = string.Empty;
                 because();
 
                 configuration.Sources.ShouldEqual(ApplicationParameters.ChocolateyCommunityFeedPushSource);
@@ -157,6 +158,7 @@ namespace chocolatey.tests.infrastructure.app.commands
             {
                 reset();
                 configuration.Sources = "";
+                configuration.PushCommand.DefaultSource = "";
                 configSettingsService.Setup(c => c.get_api_key(
                     It.Is<ChocolateyConfiguration>(config => config.Sources.is_equal_to(ApplicationParameters.ChocolateyCommunityFeedPushSourceOld)),
                     null))
@@ -169,6 +171,64 @@ namespace chocolatey.tests.infrastructure.app.commands
                     null),
                     Times.Never);
                 configuration.PushCommand.Key.ShouldNotEqual(apiKey);
+            }
+
+            [Fact]
+            public void should_set_the_source_to_defaultpushsource_if_set_and_no_explicit_source()
+            {
+                reset();
+                configuration.Sources = "";
+                configuration.PushCommand.DefaultSource = "https://localhost/default/source";
+                because();
+
+                configuration.Sources.ShouldEqual("https://localhost/default/source");
+            }
+
+            [Fact]
+            public void should_not_override_explicit_source_if_defaultpushsource_is_set()
+            {
+                reset();
+                configuration.Sources = "https://localhost/somewhere/out/there";
+                configuration.PushCommand.DefaultSource = "https://localhost/default/source";
+                because();
+
+                configuration.Sources.ShouldEqual("https://localhost/somewhere/out/there");
+            }
+
+            [Fact]
+            public void should_throw_when_defaultpushsource_is_disabled_and_no_explicit_sources()
+            {
+                reset();
+                configuration.PushCommand.DefaultSource = "disabled";
+                configuration.Sources = "";
+
+                var errorred = false;
+                Exception error = null;
+
+                try
+                {
+                    because();
+                }
+                catch (Exception ex)
+                {
+                    errorred = true;
+                    error = ex;
+                }
+
+                errorred.ShouldBeTrue();
+                error.ShouldNotBeNull();
+                error.ShouldBeType<ApplicationException>();
+                error.Message.ShouldContain("Default push source is disabled.");
+            }
+
+            [Fact]
+            public void should_continue_when_defaultpushsource_is_disabled_and_explicit_sources_passed()
+            {
+                reset();
+                configuration.Sources = "https://somewhere/out/there";
+                configuration.PushCommand.Key = "bob";
+                configuration.PushCommand.DefaultSource = "disabled";
+                because();
             }
 
             [Fact]
@@ -240,6 +300,25 @@ namespace chocolatey.tests.infrastructure.app.commands
                         Key = "https://localhost/somewhere/out/there"
                     }
                  };
+                because();
+
+                configuration.Sources.ShouldEqual("https://localhost/somewhere/out/there");
+            }
+
+            [Fact]
+            public void should_update_source_if_alias_is_passed_via_defaultpushsource()
+            {
+                reset();
+                configuration.Sources = "";
+                configuration.PushCommand.DefaultSource = "myrepo";
+                configuration.MachineSources = new List<MachineSourceConfiguration>
+                {
+                    new MachineSourceConfiguration
+                    {
+                        Name = "myrepo",
+                        Key = "https://localhost/somewhere/out/there"
+                    }
+                };
                 because();
 
                 configuration.Sources.ShouldEqual("https://localhost/somewhere/out/there");
