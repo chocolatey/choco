53694495ec6f0ff266c4a22b7329be1d770adc03
Gary Ewan Park <gep13@gep13.co.uk>
2021-12-23 08:47:11 +0000

    (maint) Remove unnecessary whitespace

--- src/chocolatey/infrastructure.app/nuget/NugetPush.cs
+++ src/chocolatey/infrastructure.app/nuget/NugetPush.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright © 2017 - 2021 Chocolatey Software, Inc
 // Copyright © 2011 - 2017 RealDimensions Software, LLC
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License at
-// 
+//
 // 	http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ namespace chocolatey.infrastructure.app.nuget
             const bool disableBuffering = false;
 
             var packageServer = new PackageServer(config.Sources, ApplicationParameters.UserAgent);
-            
+
             packageServer.SendingRequest += (sender, e) => { if (config.Verbose) "chocolatey".Log().Info(ChocolateyLoggers.Verbose, "{0} {1}".format_with(e.Request.Method, e.Request.RequestUri)); };
 
             var package = new OptimizedZipPackage(nupkgFilePath);
