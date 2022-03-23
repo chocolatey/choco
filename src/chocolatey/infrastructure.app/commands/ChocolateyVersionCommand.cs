02f9cb6de774c3bda9370a1c0545108fe8374f04
Gary Ewan Park <gep13@gep13.co.uk>
2022-03-23 12:02:30 +0000

    (#2656) Re-instate the update and version commands
    This commit re-instates the ChocolateyVersionCommand and
    ChocolateyUpdateCommand classes, however, it doesn't provide any
    implementations for the methods within the ICommand interface.  In
    addition, it also doesn't decorate the class with the CommandFor
    attribute.
    
    This has the effect that the commands still remain as unfound when
    using Chocolatey CLI open source edition, however, it means that anyone
    using an older version of the Chocolatey Licensed Extension (for
    example perpetual licensed customers) will still have a basic
    implementation of these commands that can be overridden.
    
    The Exception that is thrown when one of the implemented methods is
    executed is designed to match closely to the command not found error,
    which is actually what we _want_ folks to see.
    
    This change also has the side-effect of meaning that anyone directly
    upgrading the chocolatey package, without first upgrading the
    chocolatey.extension package, will no longer be broken after that
    installation.

---
+++ src/chocolatey/infrastructure.app/commands/ChocolateyVersionCommand.cs
@@ -0,0 +1,72 @@
+﻿// Copyright © 2017 - 2021 Chocolatey Software, Inc
+// Copyright © 2011 - 2017 RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace chocolatey.infrastructure.app.commands
+{
+    using System;
+    using System.Collections.Generic;
+    using commandline;
+    using configuration;
+    using services;
+
+    public class ChocolateyVersionCommand : ChocolateyUpgradeCommand
+    {
+        public ChocolateyVersionCommand(IChocolateyPackageService packageService) : base(packageService)
+        {
+        }
+
+        public void configure_argument_parser(OptionSet optionSet, ChocolateyConfiguration configuration)
+        {
+            throw_exception();
+        }
+
+        public void handle_additional_argument_parsing(IList<string> unparsedArguments, ChocolateyConfiguration configuration)
+        {
+            throw_exception();
+        }
+
+        public void handle_validation(ChocolateyConfiguration configuration)
+        {
+            throw_exception();
+        }
+
+        public void help_message(ChocolateyConfiguration configuration)
+        {
+            throw_exception();
+        }
+
+        public void noop(ChocolateyConfiguration configuration)
+        {
+            throw_exception();
+        }
+
+        public void run(ChocolateyConfiguration config)
+        {
+            throw_exception();
+        }
+
+        public bool may_require_admin_access()
+        {
+            return false;
+        }
+
+        private void throw_exception()
+        {
+            throw new Exception(@"Could not find a command registered that meets 'version'.
+ Try choco -? for command reference/help.");
+        }
+    }
+}
