55b96cc3e2802d29b21cefdb737392b23ee10e70
Gary Ewan Park <gep13@gep13.co.uk>
2022-05-05 13:22:19 +0100

    (#2400) Remove unnecessary TeamCity configuration
    A lot of the work that was previously being done in the TeamCity settings
    file is now done by the Recipe, and therefore can be removed.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,9 +2,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.xmlReport
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.XmlReport
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.nuGetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
@@ -19,19 +16,13 @@ object Chocolatey : BuildType({
     name = "Build"
 
     artifactRules = """
-        build_output/build_artifacts/compile/msbuild-net-4.0-results.xml
-        build_output/build_artifacts/tests/index.html
-        build_output/build_artifacts/codecoverage/Html/index.htm
-        build_output/_BuildInfo.xml
-        build_output/build.log
-        code_drop/build_artifacts/ilmerge/ilmerge.log
-        code_drop/build_artifacts/ilmerge/ilmergedll.log
-        code_drop/nuget/*.nupkg
-        src/SolutionVersion.cs
     """.trimIndent()
 
     params {
-        param("env.TEAMCITY_GIT_TAG", "")
+        param("env.vcsroot.branch", "%vcsroot.branch%")
+        param("env.Git_Branch", "%teamcity.build.vcs.branch.Chocolatey_ChocolateyVcsRoot%")
+        param("teamcity.git.fetchAllHeads", "true")
+        password("env.GITHUB_PAT", "%system.GitHubPAT%", display = ParameterDisplay.HIDDEN, readOnly = true)
     }
 
     vcs {
@@ -62,38 +53,9 @@ object Chocolatey : BuildType({
             type = "PrepareSigningEnvironment"
         }
 
-        powerShell {
-            name = "Find tag if one exists"
-            scriptMode = script {
-                content = """
-                    ${'$'}tagName = git tag -l --points-at HEAD
-
-                    if (${'$'}tagName) {
-                        Write-Host "Found tag ${'$'}tagName"
-                        Write-Host "##teamcity[setParameter name='env.TEAMCITY_GIT_TAG' value='${'$'}tagName']"
-                    } else {
-                        Write-Host "No tag found for current commit"
-                    }
-                """.trimIndent()
-            }
-        }
-
         script {
-            name = "Run Build"
-            scriptContent = "call build.official.bat"
-        }
-
-        nuGetPublish {
-            name = "Publish Packages"
-
-            conditions {
-                matches("teamcity.build.branch", "^(develop|release/.*|hotfix/.*|tags/.*)${'$'}")
-            }
-
-            toolPath = "%teamcity.tool.NuGet.CommandLine.DEFAULT%"
-            packages = "code_drop/nuget/*.nupkg"
-            serverUrl = "%env.NUGETDEV_SOURCE%"
-            apiKey = "%env.NUGETDEV_API_KEY%"
+            name = "Call Cake"
+            scriptContent = "call build.official.bat --verbosity=diagnostic --target=CI"
         }
     }
 
@@ -104,11 +66,6 @@ object Chocolatey : BuildType({
     }
 
     features {
-        xmlReport {
-            reportType = XmlReport.XmlReportType.NUNIT
-            rules = "build_output/build_artifacts/tests/test-results.xml"
-        }
-
         pullRequests {
             provider = github {
                 authType = token {
