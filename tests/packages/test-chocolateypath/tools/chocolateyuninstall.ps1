30265be62087cbe0692cdeccf43dda8b7b4a3e72
AdmiringWorm <kim@chocolatey.io>
2022-10-18 15:14:16 +0200

    (#2860) Add e2e tests for new PS cmdlet

--- tests/packages/test-chocolateypath/tools/chocolateyuninstall.ps1
+++ tests/packages/test-chocolateypath/tools/chocolateyuninstall.ps1
@@ -2,8 +2,6 @@
 
 $packagePath = Get-ChocolateyPath -PathType 'PackagePath'
 $installPath = Get-ChocolateyPath -PathType 'InstallPath'
-$toolsPath   = Get-ChocolateyPath -PathType 'ToolsPath'
 
 Write-Host "Package Path in Uninstall Script: $packagePath"
 Write-Host "Install Path in Uninstall Script: $installPath"
-Write-Host "Tools Path in Uninstall Script: $toolsPath"
