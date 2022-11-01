f9ebe5a399440f686b99cc8fcc888577059fa8db
Gary Ewan Park <gep13@gep13.co.uk>
2022-11-01 08:22:05 +0000

    (build) Switch to running all tests
    For the time being, while we work through the next scope of work, we
    want to make sure that we are running _all_ tests. This will ensure
    that we don't miss anything when bringing in some of the planned
    breaking changes in the next release.
    
    All tests were already running on GitHub Actions when doing the nightly
    test build, however, this makes it so they run on every CI build.  This
    change can be backed out, once we have shipped the next release.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -55,7 +55,7 @@ object Chocolatey : BuildType({
 
         script {
             name = "Call Cake"
-            scriptContent = "call build.official.bat --verbosity=diagnostic --target=CI"
+            scriptContent = "call build.official.bat --verbosity=diagnostic --target=CI --testExecutionType=all --shouldRunOpenCover=false"
         }
     }
 
