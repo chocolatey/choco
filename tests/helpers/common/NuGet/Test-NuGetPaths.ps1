ec90407328f271db3ac24c870bc2056f99319784
Cory Knox <me@coryknox.dev>
2023-01-23 13:22:29 -0800

    (#2738) Tidy up NuGet path tests
    NuGet Path tests currently get conflated with the tests they're within
    (when run in Test Kitchen locally). This commit puts them inside their
    own Context Block so they're easier to identify. It also removes some
    code that didn't actually do anything.

--- tests/helpers/common/NuGet/Test-NuGetPaths.ps1
+++ tests/helpers/common/NuGet/Test-NuGetPaths.ps1
@@ -1,16 +1,21 @@
 function Test-NuGetPaths {
-    $script:NuGetCleared = $false
-    $NuGetPathsToCheck = Get-NuGetPaths
-    $ChocolateyNuGetPath = "$(Get-TempDirectory)chocolatey-invalid"
-
-    It 'Did not create <_> directory' -ForEach $NuGetPathsToCheck -Skip:((-not $env:TEST_KITCHEN)) {
-        if (-not $script:NuGetCleared) {
-            Set-ItResult -Skipped -Because 'NuGet configurations were not removed before running tests'
+    Context 'NuGet directory tests' {
+        BeforeDiscovery {
+            $NuGetPathsToCheck = Get-NuGetPaths
+            $ChocolateyNuGetPath = "$(Get-TempDirectory)chocolatey-invalid"
         }
-        $_ | Should -Not -Exist
-    }
 
-    It "Did not create <_>" -ForEach $ChocolateyNuGetPath {
-        $_ | Should -Not -Exist
+        AfterAll {
+            $script:NuGetCleared = $null
+        }
+
+        It 'Did not create <_> directory' -ForEach $NuGetPathsToCheck -Skip:((-not $env:TEST_KITCHEN)) {
+            $script:NuGetCleared | Should -BeTrue -Because 'NuGet configurations were not removed before running tests'
+            $_ | Should -Not -Exist
+        }
+
+        It "Did not create <_>" -ForEach $ChocolateyNuGetPath {
+            $_ | Should -Not -Exist
+        }
     }
 }
