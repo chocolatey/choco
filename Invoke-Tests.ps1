15a26ef21aa47f2e9704775045db1bc877cbf4da
Cory Knox <me@coryknox.dev>
2023-03-27 15:10:14 -0700

    (#3088) Don't loop over null hook script variables
    PowerShell 2 had the behaviour that foreach would iterate over a null
    variable
    (https://devblogs.microsoft.com/powershell/new-v3-language-features/).
    This has resulted in an issue if no hook scripts are passed in, then
    chocolateyScriptRunner would cause an error. This update changes this to
    do a nullcheck like we already had for the packageScript parameter.

--- Invoke-Tests.ps1
+++ Invoke-Tests.ps1
@@ -31,7 +31,7 @@ $TestsLocation = Join-Path $PSScriptRoot tests
 $MaxFileNameLength = 110
 $LongFiles = Get-ChildItem $TestsLocation -Recurse |
     Where-Object { ($_.FullName.Length - $TestsLocation.Length) -gt $MaxFileNameLength } |
-    Select-Object -Property @{Name = 'RelativePath' ; Expression = { $_.FullName.Replace($TestsLocation, [string]::Empty)}}, @{ Name = 'ReductionNeeded' ; Expression = { $_.FullName.Length - $TestsLocation.Length - $MaxFileNameLength } }
+        Select-Object -Property @{Name = 'RelativePath' ; Expression = { $_.FullName.Replace($TestsLocation, [string]::Empty) } }, @{ Name = 'ReductionNeeded' ; Expression = { $_.FullName.Length - $TestsLocation.Length - $MaxFileNameLength } }
 
 if ($LongFiles) {
     Write-Host "Tests' file paths may be too long for Test Kitchen use. Please shorten file names or paths:"
@@ -117,6 +117,9 @@ try {
                     'VMOnly'
                 }
             )
+        }
+        Should     = @{
+            ErrorAction = 'Continue'
         }
     }
 
