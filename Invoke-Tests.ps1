c53df983a0d136a6f562674196b27f6e6a4462f8
Cory Knox <me@coryknox.dev>
2023-05-12 09:58:47 -0700

    (#1174) Add non-normalized packages for tests
    Add packages with a non-normalized version number. These are the packed 
    nupkgs as we can't `choco pack` them with v2.x. Updated the 
    `Invoke-Tests.ps1` file in order to also copy these files to the test 
    packages directory.

--- Invoke-Tests.ps1
+++ Invoke-Tests.ps1
@@ -1,4 +1,4 @@
-﻿#Requires -Module @{ ModuleName = 'pester'; ModuleVersion = '5.3.1' }
+#Requires -Module @{ ModuleName = 'pester'; ModuleVersion = '5.3.1' }
 #Requires -RunAsAdministrator
 <#
     .SYNOPSIS
@@ -50,7 +50,8 @@ else {
 if (-not (Test-Path "$TestPath/packages") -or -not $SkipPackaging) {
     $null = New-Item -Path "$TestPath/packages" -ItemType Directory -Force
     # Get and pack packages
-    $nuspecs = Get-ChildItem -Path $PSScriptRoot/src/chocolatey.tests.integration, $PSScriptRoot/tests/packages -Recurse | Where-Object Name -Like '*.nuspec'
+    $nuspecs = Get-ChildItem -Path $PSScriptRoot/src/chocolatey.tests.integration, $PSScriptRoot/tests/packages -Recurse -Include *.nuspec
+    Get-ChildItem -Path $PSScriptRoot/tests/packages -Recurse -Include *.nupkg | Copy-Item -Destination "$TestPath/packages"
 
     foreach ($file in $nuspecs) {
         Write-Host "Packaging $file"
