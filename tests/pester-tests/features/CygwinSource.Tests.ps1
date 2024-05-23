704cd654f533d10048fb518925184fe0f8e89741
Gary Ewan Park <gep13@gep13.co.uk>
2024-05-23 07:37:06 -0700

    (#23) Add tests to handle install all
    This commit adds some Pester tests to ensure that any attempts to run
    the choco install all command is correctly handled when targeting
    an alternative source.  This is simply asserting that there is an
    error exit code, and that the correct output is displayed.
    
    Some of the new tests are specifically set to only run on Test-Kitchen,
    as there is the potential for changes to happen on the host machine,
    and we don't want to impact people.

---
+++ tests/pester-tests/features/CygwinSource.Tests.ps1
@@ -0,0 +1,30 @@
+﻿Import-Module helpers/common-helpers
+
+# This is skipped when not run in CI because it modifies the local system.
+Describe "Cygwin Source" -Tag Chocolatey, CygwinSource -Skip:(-not $env:TEST_KITCHEN) {
+    BeforeAll {
+        Initialize-ChocolateyTestInstall
+        New-ChocolateyInstallSnapshot
+        Enable-ChocolateySource -Name hermes-setup
+        $null = Invoke-Choco install cygwin
+    }
+
+    AfterAll {
+        $null = Invoke-Choco uninstall cygwin --remove-dependencies
+        Remove-ChocolateyTestInstall
+    }
+
+    Context "install all" {
+        BeforeAll {
+            $Output = Invoke-Choco install all --source=cygwin
+        }
+
+        It 'Exits with exit code (1)' {
+            $Output.ExitCode | Should -Be 1 -Because $Output.String
+        }
+
+        It 'Outputs exception' {
+            $Output.Lines | Should -Contain "Alternative sources do not allow the use of the 'all' package name/keyword." -Because $Output.String
+        }
+    }
+}
