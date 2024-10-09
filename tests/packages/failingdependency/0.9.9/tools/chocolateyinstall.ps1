20ef232e886ac09fd7e9d6f43ade7deb46b72bbd
Cory Knox <me@coryknox.dev>
2024-10-09 09:43:22 -0700

    (maint) Update Test Packages to match internal
    This commit updates the test packages on the repository to match the
    packages hosted on the internal repository.

---
+++ tests/packages/failingdependency/0.9.9/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿"Creating Runtime File"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+"Test File" | Out-File "$toolsDir\test-file.txt"
+
+Write-Error "This should fail!"
+$env:ChocolateyExitCode = '15608'
