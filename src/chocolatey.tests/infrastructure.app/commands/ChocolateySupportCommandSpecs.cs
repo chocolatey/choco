416d3523ac335759b33dfb3729b494c5fe4566ad
Gary Ewan Park <gep13@gep13.co.uk>
2025-05-30 13:14:37 +0100

    (#3668) Add choco support command
    This commit introduces a new choco support command for Chocolatey CLI.
    This command already exists for Chocolatey Licensed Extension, but the
    decision was made to also introduce it into open-source, for
    consistency reasons.  The output from this version of the command is
    different to what is shown when you have a license in play, as it
    points the user to the open-source resources, rather than to reach out
    to the support team.
    
    Also included is some unit tests and some pester tests, to ensure that
    the command is working as expected.

---
+++ src/chocolatey.tests/infrastructure.app/commands/ChocolateySupportCommandSpecs.cs
@@ -0,0 +1,137 @@
+﻿// Copyright © 2017 - 2025 Chocolatey Software, Inc
+// Copyright © 2011 - 2017 RealDimensions Software, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using chocolatey.infrastructure.app.attributes;
+using chocolatey.infrastructure.app.commands;
+using chocolatey.infrastructure.app.configuration;
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace chocolatey.tests.infrastructure.app.commands
+{
+    public class ChocolateySupportCommandSpecs
+    {
+        [ConcernFor("support")]
+        public abstract class ChocolateySupportCommandSpecsBase : TinySpec
+        {
+            protected ChocolateySupportCommand Command;
+            protected ChocolateyConfiguration Configuration = new ChocolateyConfiguration();
+
+            public override void Context()
+            {
+                Command = new ChocolateySupportCommand();
+            }
+        }
+
+        public class When_Implementing_Command_For : ChocolateySupportCommandSpecsBase
+        {
+            private List<CommandForAttribute> _results;
+
+            public override void Because()
+            {
+                _results = Command.GetType().GetCustomAttributes<CommandForAttribute>().ToList();
+            }
+
+            [Fact]
+            public void Should_Have_Expected_Number_Of_Commands()
+            {
+                _results.Should().HaveCount(1);
+            }
+
+            [InlineData("support")]
+            public void Should_Implement_Expected_Command(string name)
+            {
+                _results.Should().ContainSingle(r => r.CommandName == name);
+            }
+
+            [Fact]
+            public void Should_Specify_Expected_Version_For_All_Commands()
+            {
+                _results.Should().AllSatisfy(r => r.Version.Should().Be("2.5.0"));
+            }
+        }
+
+        public class When_Noop_Is_Called : ChocolateySupportCommandSpecsBase
+        {
+            public override void Because()
+            {
+                Command.DryRun(Configuration);
+            }
+
+            [Fact]
+            public void Should_log_a_message()
+            {
+                MockLogger.Verify(l => l.Info(It.IsAny<string>()), Times.AtLeastOnce);
+            }
+
+            [Fact]
+            public void Should_log_the_message_we_expect()
+            {
+                var messages = MockLogger.MessagesFor(LogLevel.Info);
+                messages.Should().ContainSingle();
+                messages[0].Should().Contain("Unfortunately, we are unable to provide private support");
+            }
+        }
+
+        public class When_Run_Is_Called : ChocolateySupportCommandSpecsBase
+        {
+            public override void Because()
+            {
+                Command.Run(Configuration);
+            }
+
+            [Fact]
+            public void Should_log_a_message()
+            {
+                MockLogger.Verify(l => l.Info(It.IsAny<string>()), Times.AtLeastOnce);
+            }
+
+            [Fact]
+            public void Should_log_the_message_we_expect()
+            {
+                var messages = MockLogger.MessagesFor(LogLevel.Info);
+                messages.Should().ContainSingle();
+                messages[0].Should().Contain("Unfortunately, we are unable to provide private support");
+            }
+        }
+
+        public class When_Help_Is_Called : ChocolateySupportCommandSpecsBase
+        {
+            public override void Because()
+            {
+                Command.HelpMessage(Configuration);
+            }
+
+            [Fact]
+            public void Should_log_a_message()
+            {
+                MockLogger.Verify(l => l.Info(It.IsAny<string>()), Times.AtLeastOnce);
+            }
+
+            [Fact]
+            public void Should_log_the_message_we_expect()
+            {
+                var messages = MockLogger.MessagesFor(LogLevel.Info);
+                messages.Should().HaveCount(2);
+                messages[0].Should().Contain("Support Command");
+                messages[1].Should().Contain("As an open-source user of Chocolatey CLI, we are not able to");
+            }
+        }
+    }
+}
---
+++ tests/pester-tests/commands/choco-support.Tests.ps1
@@ -0,0 +1,62 @@
+﻿Import-Module helpers/common-helpers
+
+Describe "choco support" -Tag Chocolatey, SupportCommand {
+    BeforeDiscovery {
+        $HasLicensedExtension = Test-PackageIsEqualOrHigher -PackageName 'chocolatey.extension' -Version '6.0.0'
+    }
+
+    BeforeAll {
+        Remove-NuGetPaths
+        Initialize-ChocolateyTestInstall
+        New-ChocolateyInstallSnapshot
+    }
+
+    AfterAll {
+        Remove-ChocolateyTestInstall
+    }
+
+    Context "Support" {
+        BeforeAll {
+            $Output = Invoke-Choco support
+        }
+
+        It "Exits successfully (0)" {
+            $Output.ExitCode | Should -Be 0 -Because $Output.String
+        }
+
+        It "Reports support options" {
+            $ExpectedOutput = if($HasLicensedExtension) {
+                "Howdy, you have access to private support channels."
+            } else {
+                "Unfortunately, we are unable to provide private support for"
+            }
+            $Output.Lines | Should -Contain $ExpectedOutput -Because $Output.String
+        }
+    }
+
+    Context "Help Documentation (<_>)" -ForEach @("--help", "-?", "-help") {
+        BeforeAll {
+            $Output = Invoke-Choco support $_
+        }
+
+        It "Exits successfully (0)" {
+            $Output.ExitCode | Should -Be 0 -Because $Output.String
+        }
+
+        It "Outputs Help for Support" {
+            $Output.String | Should -Match "Support Command" -Because $Output.String
+        }
+
+        It "Outputs Help for Support" {
+            $ExpectedOutput = if($HasLicensedExtension) {
+                "As a licensed customer, you can reach out to"
+            } else {
+                "As an open-source user of Chocolatey CLI, we are not able to"
+            }
+            $Output.Lines | Should -Contain $ExpectedOutput -Because $Output.String
+        }
+    }
+
+    # This needs to be the last test in this block, to ensure NuGet configurations aren't being created.
+    Test-NuGetPaths
+}
