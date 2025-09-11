5040369f35b469ba70215bb9e601c47b67c5e760
Cory Knox <me@coryknox.dev>
2025-09-11 07:40:54 -0700

    (#3681) Add tests for System Account variables
    This commit adds Pester tests to run `refreshenv` as the System account
    and ensure that it does not inadvertently pick up the wrong temp
    variables.

---
+++ tests/helpers/common/ConvertTo-Base64String.ps1
@@ -0,0 +1,14 @@
+﻿function ConvertTo-Base64String {
+    <#
+        .Synopsis
+            Helper function to Convert a string to a Base64 encoded string.
+    #>
+    [CmdletBinding()]
+    param(
+        # The string to be converted to base64.
+        [Parameter(ValueFromPipeline)]
+        [string]$InputObject
+    )
+    $bytes = [system.text.encoding]::Unicode.GetBytes($InputObject)
+    [Convert]::ToBase64String($bytes)
+}
